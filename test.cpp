0839eaab6beeb342bc388623da04efd69674c884
icestream32 <1601334224@qq.com>
2022-01-15 15:30:33 +0800

    master

--- test.cpp
+++ test.cpp
@@ -1,21 +1,12 @@
 #include<iostream>
 using namespace std;
 
-
-
-
-
 void biger_num(int num_frist,int num_second);
-
 
 int main(){
     cout<<"HelloWorld!"<<endl;
     return 0;
 }
-
-
-
-
 
 void biger_num(int num_frist,int num_second){
     int biger_num = (num_frist > num_second) ? num_second : num_frist;
