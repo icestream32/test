519669aa5769fa43e91d549eaef1ee2fd7bf0493
icestream32 <1601334224@qq.com>
2022-01-15 15:26:06 +0800

    frist commit for feature_bijiaodaxiao

--- test.cpp
+++ test.cpp
@@ -1,14 +1,14 @@
 #include<iostream>
 using namespace std;
 
-void cha(int num1,int num2);
+void biger_num(int num_frist,int num_second);
 
 int main(){
     cout<<"HelloWorld!"<<endl;
     return 0;
 }
 
-void cha(int num1,int num2){
-    int cha = num1 + num2;
-    cout<<cha<<endl;
+void biger_num(int num_frist,int num_second){
+    int biger_num = (num_frist > num_second) ? num_second : num_frist;
+    cout<<biger_num<<endl;
 }
