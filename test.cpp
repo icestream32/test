0b35bddd73aff73d763b4fb25183724901266f61
icestream32 <1601334224@qq.com>
2022-01-15 15:30:12 +0800

    feibonaqishulie

--- test.cpp
+++ test.cpp
@@ -1,14 +1,24 @@
-#include<iostream>
-using namespace std;
-
-void cha(int num1,int num2);
-
-int main(){
-    cout<<"HelloWorld!"<<endl;
-    return 0;
-}
-
-void cha(int num1,int num2){
-    int cha = num1 + num2;
-    cout<<cha<<endl;
-}
+#include<stdio.h>
+int main ()
+{
+	int a=1,b=2,c,n;
+	scanf("%d",&n);
+	if (n==1)
+	{
+		printf("1\n");
+		return 0;
+	}
+	if (n==2)
+	{
+		printf("2\n");
+		return 0;
+	}
+	for (int i=1;i<=n-3;i++)
+	{
+		c=a+b;
+		a=b;
+		b=c;
+	}
+	printf("%d",b);
+	return 0;
+} 
