8cfdbe48fc9599b4dd43dd2534de6217e1ea983f
icestream32 <1601334224@qq.com>
2022-01-15 15:15:00 +0800

    frist commit for feature_jianfa

--- test.cpp
+++ test.cpp
@@ -1,14 +1,14 @@
 #include<iostream>
 using namespace std;
 
-void he(int num1,int num2);
+void cha(int num1,int num2);
 
 int main(){
     cout<<"HelloWorld!"<<endl;
     return 0;
 }
 
-void he(int num1,int num2){
-    int he = num1 + num2;
-    cout<<he<<endl;
+void cha(int num1,int num2){
+    int cha = num1 + num2;
+    cout<<cha<<endl;
 }
