78445d0305d2e7748e95635e7e5838dfeaf58108
icestream32 <1601334224@qq.com>
2022-01-15 15:27:20 +0800

    second commit for feature_bijiaodaxiao

--- test.cpp
+++ test.cpp
@@ -1,12 +1,21 @@
 #include<iostream>
 using namespace std;
 
+
+
+
+
 void biger_num(int num_frist,int num_second);
+
 
 int main(){
     cout<<"HelloWorld!"<<endl;
     return 0;
 }
+
+
+
+
 
 void biger_num(int num_frist,int num_second){
     int biger_num = (num_frist > num_second) ? num_second : num_frist;
